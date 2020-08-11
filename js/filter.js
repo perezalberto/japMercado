07ae633bf43545a9eec1b1f28e646d70b58632be
Alberto Perez <5889896+perezalberto@users.noreply.github.com>
2020-08-11 19:12:03 -0300

    Página de productos

---
+++ js/filter.js
@@ -0,0 +1,83 @@
+const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_DESC_BY_NAME = "ZA";
+const ORDER_BY_PROD_COUNT = "Cant.";
+
+function sortList(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name < b.name) { return -1; }
+            if (a.name > b.name) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_DESC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name > b.name) { return -1; }
+            if (a.name < b.name) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_PROD_COUNT) {
+        result = array.sort(function (a, b) {
+            let aCount = parseInt(a.productCount || a.soldCount);
+            let bCount = parseInt(b.productCount || b.soldCount);
+
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
+// list filter
+function listFilterComponent(data,listener) {
+
+    var minCount = undefined;
+    var maxCount = undefined;
+
+    document.getElementById("sortAsc").addEventListener("click", function () {
+        listener(sortList(ORDER_ASC_BY_NAME,data));
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function () {
+        listener(sortList(ORDER_DESC_BY_NAME,data));
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function () {
+        listener(sortList(ORDER_BY_PROD_COUNT,data));
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function () {
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+
+        minCount = undefined;
+        maxCount = undefined;
+
+        listener(data);
+    });
+
+    document.getElementById("rangeFilterCount").addEventListener("click", function () {
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+        //de productos por categoría.
+        minCount = document.getElementById("rangeFilterCountMin").value;
+        maxCount = document.getElementById("rangeFilterCountMax").value;
+
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0) {
+            minCount = parseInt(minCount);
+        }
+        else {
+            minCount = undefined;
+        }
+
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0) {
+            maxCount = parseInt(maxCount);
+        }
+        else {
+            maxCount = undefined;
+        }
+
+        listener(data);
+    });
+}
